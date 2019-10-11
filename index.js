7da3ca4e0884ba00995800448a55bcb750b88fb7
arifikhsan <arif.ikhsanudin.developer@gmail.com>
2019-10-11 18:39:29 +0700

    gitignore

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello Node JS"));
+app.get("/", (_, res) => res.send("Hello Node JS"));
 app.listen(port, () => console.log(`Expressjs app listening on port ${port}!`));
