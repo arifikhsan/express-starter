f3598957786e507ca31b7052e9dd126995cd09cd
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2019-10-11 18:35:59 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => res.send("Hello Node JS"));
+app.listen(port, () => console.log(`Expressjs app listening on port ${port}!`));
